ef4e81f09dca2d0eb065fdf62cbd80f6155c3f49
Seb Conway <103641460@student.swin.edu.au>
2021-07-26 16:02:04 +1000

    Finished up to get customer string, all works

--- Tutorials/Tutorials.sql
+++ Tutorials/Tutorials.sql
@@ -1,5 +1,15 @@
---View all 
+-- View all databases on the server
 
 SELECT name, database_id, create_date
 From sys.databases ;
 GO
+
+-- Viewing tables in current database
+SELECT * FROM INFORMATION_SCHEMA.tables
+
+-- Display the list 
+
+
+
+
+create database TSQL_Assignment
